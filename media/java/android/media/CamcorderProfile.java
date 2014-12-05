1adb01f0c43ddbb9989da72134070bf037d8553b
Surajit Podder <spodder@codeaurora.org>
2014-12-05 18:58:24 +0530

    frameworks/base: Add HVGA camcorder profile
    Added entry for HVGA resolution in camcorder profile
    
    Change-Id: I7f4e93a9ae9ea4063e0b32298931a3893ee7dd4f

--- media/java/android/media/CamcorderProfile.java
+++ media/java/android/media/CamcorderProfile.java
@@ -125,6 +125,11 @@ public class CamcorderProfile
      */
     public static final int QUALITY_HVGA = 19;
 
+    /** @hide
+     * Quality level corresponding to 4K DCI resolution
+     */
+    public static final int QUALITY_HVGA = 18;
+
     // Start and end of quality list
     private static final int QUALITY_LIST_START = QUALITY_LOW;
     private static final int QUALITY_LIST_END = QUALITY_HVGA;
