8e121e26d82fa16ff8094c7a46494f89ecad359b
B--B <dark.schneider.ds@gmail.com>
2015-08-03 00:51:05 +0200

    Force Expanded Notifications (1/2)
        This will force expanded notifications for all apps that support
        expanded notifications

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2824,6 +2824,12 @@ public final class Settings {
         public static final String RECENTS_SEARCH_BAR = "recents_search_bar";
 
         /**
+         * Force expanded notifications on all apps that support it.
+         * @hide
+         */
+        public static final String FORCE_EXPANDED_NOTIFICATIONS = "force_expanded_notifications";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
