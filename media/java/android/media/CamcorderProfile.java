ffee2f95a185b25e5d23b4e19655cf9f12286673
B--B <dark.schneider.ds@gmail.com>
2016-02-23 01:31:30 +0100

    Remove a duplicate

--- media/java/android/media/CamcorderProfile.java
+++ media/java/android/media/CamcorderProfile.java
@@ -123,11 +123,6 @@ public class CamcorderProfile
     /** @hide
      * Quality level corresponding to 4K DCI resolution
      */
-    public static final int QUALITY_HVGA = 19;
-
-    /** @hide
-     * Quality level corresponding to 4K DCI resolution
-     */
     public static final int QUALITY_HVGA = 18;
 
     // Start and end of quality list
